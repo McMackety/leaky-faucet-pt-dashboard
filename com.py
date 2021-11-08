b371f078e6373b4d334b020c58b04346919d0258
Chase MacDonnell <macdonnell.chase@gmail.com>
2021-11-08 16:46:07 -0500

    Fix PT Code

--- com.py
+++ com.py
@@ -1,28 +1,40 @@
 import serial
 import threading
-import time
+import io
 
-com_port = "/dev/tty.URT1"
+com_port = "/dev/tty.usbmodem14401"
 baud_rate = 115200
 
 class RocketSerial(threading.Thread):
     def __init__(self):
-        self.data = ["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "0.0"]
+        self.data = []
+        self.mutex = threading.Lock()
         self.should_stop = False
+        self.file = io.open("log.csv", "a")
         self.serial = serial.Serial(com_port, baud_rate, timeout=1)
         threading.Thread.__init__(self)
 
     def read_data(self):
-        return self.data
+        self.mutex.acquire()
+        new_data = self.data
+        self.mutex.release()
+        return new_data
 
     def stop(self):
+        self.mutex.acquire()
         self.should_stop = True
+        self.mutex.release()
 
-    #def run(self):
-        #while True:
-            #if self.should_stop:
-                #return
-            #data = self.serial.readline()
-            #array = data.decode("utf-8") .split(',')
-            #array.pop()
-            #self.data = array
+    def run(self):
+        while True:
+            self.mutex.acquire()
+            if self.should_stop:
+                return
+            data = self.serial.readline()
+            data = data.decode()
+            data.strip()
+            array = data.split(',')
+            if len(array) == 9 and array[0] != "0":
+                self.file.write(data)
+            self.data = array
+            self.mutex.release()
